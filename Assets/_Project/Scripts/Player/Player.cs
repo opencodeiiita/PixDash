3096409bf50443fb64b59dc98c1189aa1a47ba75
KushagraSahu-01 <luckysahu1224@gmail.com>
2025-12-30 16:13:21 +0530

    Animator added

--- Assets/_Project/Scripts/Player/Player.cs
+++ Assets/_Project/Scripts/Player/Player.cs
@@ -7,11 +7,12 @@ public class Player : MonoBehaviour
     public float jumpForce = 10f;
     public Transform groundCheck;
     public LayerMask groundLayer;
+    public Animator animator;
     public float groundRadius = 0.2f;
-
+    public float fallCheck = 2f;
     private Rigidbody2D rb;
     private float horizontal;
-    private bool isGrounded;
+    private bool isGrounded,isFalling=false;
 
     private bool FacingRight = true;
 
@@ -24,7 +25,7 @@ public class Player : MonoBehaviour
     {
         horizontal = Keyboard.current.aKey.isPressed ? -1 :
                      Keyboard.current.dKey.isPressed ? 1 : 0;
-
+        animator.SetFloat("IsRunning", Mathf.Abs(horizontal));
         if((horizontal==-1&&FacingRight==true)||(horizontal==1&&FacingRight==false))
         {
             Flip();
@@ -34,9 +35,17 @@ public class Player : MonoBehaviour
             groundRadius,
             groundLayer
         );
+        animator.SetBool("IsGrounded", isGrounded);
         if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isFalling = false;
+            animator.SetBool("IsFalling", isFalling);
+        }
+        if (!isFalling && rb.linearVelocityY < fallCheck)
+        {
+            isFalling = true;
+            animator.SetBool("IsFalling", isFalling);
         }
     }
 
